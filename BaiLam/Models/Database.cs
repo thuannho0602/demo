9d604ab4242e36b621ca7373b5102709c55ba18a
thuannguyen <thuannho0602@gmail.com>
2023-02-27 15:04:48 +0700

    add new

---
+++ BaiLam/Models/Database.cs
@@ -0,0 +1,24 @@
+﻿using BaiLam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+
+namespace BaiLam.BAL
+{
+    public class Database
+    {
+        DatatProvide _provider = new DatatProvide();
+        public Database()
+        {
+            _provider.Connect();
+        }
+        public void Disconnect()
+        {
+            _provider.Disconnect();
+        }
+      
+    }
+}
---
+++ BaiLam/Models/DatatProvide.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BaiLam.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BaiLam.Models
+{
+    public class DatatProvide
+    {
+        protected static string _connectionString;
+        public static String ConnectionString
+        {
+            get { return _connectionString; }
+            set { _connectionString = value; }
+        }
+        protected SqlConnection cn;
+        protected SqlDataAdapter da;
+        protected SqlConnection cmd;
+        protected readonly DataSet dt;
+        public DatatProvide()
+        {
+
+        }
+        public void Connect()
+        {
+            cn = new SqlConnection(ConnectionString);
+            cn.Open();
+        }
+        public void Disconnect()
+        {
+            cn.Close();
+        }
+        public DataTable Qu
+       
+
+    }
+}
